0dd656da917bc13ef9df9bfe32531404794b6142
AlvinFerdinand <156291369+AlvinFerdinand@users.noreply.github.com>
2025-02-21 12:47:41 +0700

    Pesan commit

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://resto-ezyaumbjc-alvins-projects-82d767c5.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
